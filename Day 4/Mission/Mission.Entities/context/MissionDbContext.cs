32185c663ba7d3242579bf10f2b31f0fdaa386f2
PanchalAnshul <anshulpanchal0909@gmail.com>
2025-07-09 17:41:46 +0530

    Add Day 4 project files

---
+++ Day 4/Mission/Mission.Entities/context/MissionDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mission.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission.Entities.context
+{
+    public class MissionDbContext : DbContext
+    {
+        public MissionDbContext(DbContextOptions<MissionDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+            // ✅ Tell EF to use "Users" table name
+            modelBuilder.Entity<User>().ToTable("Users");
+
+            modelBuilder.Entity<User>().HasData(new User()
+            {
+                Id = 1,
+                FirstName = "Anshul",
+                LastName = "Panchal",
+                EmailAddress = "anshulpanchal9099@gmail.com",
+                UserType = "admin",
+                Password = "Anshul@123",
+                PhoneNumber = "9876543210",
+                CreatedDate = new DateTime(2019, 1, 1, 0, 0, 0, DateTimeKind.Utc),
+                ModifiedDate = DateTime.UtcNow,
+                UserImage = "",
+                IsDeleted = false
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
