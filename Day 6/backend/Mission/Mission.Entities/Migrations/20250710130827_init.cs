44ca7521a9cc4258f25f0790a6953a564fe2be51
PanchalAnshul <anshulpanchal0909@gmail.com>
2025-07-11 16:59:04 +0530

    Frontend Mission with Mission User and Mission Theme

---
+++ Day 6/backend/Mission/Mission.Entities/Migrations/20250710130827_init.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EmailAddress", "FirstName", "LastName" },
+                values: new object[] { "preksha@gmail.com", "Preksha", "Divaraniya" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EmailAddress", "FirstName", "LastName" },
+                values: new object[] { "shri@gmail.com", "shri", "mehta" });
+        }
+    }
+}
---
+++ Day 6/backend/Mission/Mission.Entities/Migrations/20250710154919_init2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EmailAddress", "FirstName", "LastName", "Password" },
+                values: new object[] { "anshul@gmail.com", "Anshul", "Panchal", "123456" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EmailAddress", "FirstName", "LastName", "Password" },
+                values: new object[] { "preksha@gmail.com", "Preksha", "Divaraniya", "123" });
+        }
+    }
+}
